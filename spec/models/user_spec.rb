58d1525f89e03c07d536e60040c4242234632282
vladimirov-k-a2015 <sheyan.anton@gmail.com>
2016-03-04 13:13:25 +0200

    4 in 1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,7 @@ require 'spec_helper'
 
 describe User do
 
+
   before do 
    @user = User.new(name: "Example User", email: "user@example.com",
                              password: "foobar", password_confirmation: "foobar") 
